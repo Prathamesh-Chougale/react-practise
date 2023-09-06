cf2f95e464ad4fb2d0c7ddb86418e2a16b0da3c3
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-06 18:20:37 +0530

    saving the react work

---
+++ react-project/src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <Message />{/* This is the component we created in the previous step.*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-project/src/Message.tsx
@@ -0,0 +1,9 @@
+
+function Message(){
+    const message = "";
+    if(message)
+        return <h1>{message}</h1>
+    return <h1>no message found</h1>
+}
+
+export default Message;
