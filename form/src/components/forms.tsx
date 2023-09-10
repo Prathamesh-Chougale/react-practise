4647ca928c782d6b8fc6eb7216ff54b1c187beb8
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-10 23:50:00 +0530

    completed some work of react form

--- form/src/components/forms.tsx
+++ form/src/components/forms.tsx
@@ -1,23 +1,26 @@
 // import React,{FormEvent} from "react";
 
-import { FormEvent, useRef } from "react";
+// import { FormEvent, useRef } from "react";
+import { useForm, FieldValues } from "react-hook-form";
 
-export const forms = () => {
-  const useName = useRef<HTMLInputElement>(null);
-  const useAge = useRef<HTMLInputElement>(null);
-  const person = { name: "prathamesh", age: 21 };
+function Forms() {
+  // const useName = useRef<HTMLInputElement>(null);
+  // const useAge = useRef<HTMLInputElement>(null);
+  // const person = { name: "prathamesh", age: 21 };
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (useName.current !== null) person.name = useName.current.value;
-    if (useAge.current !== null) person.age = Number(useAge.current.value);
-    console.log(person);
-  };
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   if (useName.current !== null) person.name = useName.current.value;
+  //   if (useAge.current !== null) person.age = Number(useAge.current.value);
+  //   console.log(person);
+  // };
 
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
     <form
       onSubmit={
-        handleSubmit
+        handleSubmit(onSubmit)
         // {event.preventDefault(); //this is used to prevent the default behaviour of the form
         // console.log("form submitted");}
       }
@@ -26,19 +29,29 @@ export const forms = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={useName} id="name" type="text" className="form-control" />
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           age
         </label>
-        <input ref={useAge} id="name" type="number" className="form-control" />
+        <input
+          {...register("age")}
+          id="name"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
   );
-};
+}
 
-export default forms;
+export default Forms;
