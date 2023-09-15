d96e78844e5366cbf3eb989cef59d7a6e7f2a026
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-15 21:06:45 +0530

    saving the react work

--- backend_practise/src/App.tsx
+++ backend_practise/src/App.tsx
@@ -64,9 +64,15 @@ function App() {
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {user.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item  d-flex justify-content-between"
+          >
+            {user.name}{" "}
+            <button className="btn btn-outline-danger">Delete</button>
+          </li>
         ))}
       </ul>
     </>
