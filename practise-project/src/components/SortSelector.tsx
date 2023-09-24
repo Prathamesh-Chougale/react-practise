69a5a2197faa13e35d8311b53942689d93377d81
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-24 16:34:56 +0530

    added sort order sorting

--- practise-project/src/components/SortSelector.tsx
+++ practise-project/src/components/SortSelector.tsx
@@ -1,23 +1,39 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Realease date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {/* {selectedPlatform?.name || "Platform"} */}
-        Order of Relevance
+        Order by : {currentSortOrder?.label || "Relavance"}
       </MenuButton>
       <MenuList>
-        {/* {data.map((p) => (
-          <MenuItem onClick={() => onSelectPlatform(p)} key={p.id}>
-            {p.name}
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
+          >
+            {order.label}
           </MenuItem>
-        ))} */}
-        <MenuItem>1</MenuItem>
-        <MenuItem>1</MenuItem>
-        <MenuItem>1</MenuItem>
-        <MenuItem>1</MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- practise-project/src/services/image-url.ts
+++ practise-project/src/services/image-url.ts
@@ -1,4 +1,6 @@
 const CroppedImage = (image: string) => {
+    if (!image) return '';
+
     return image.replace("/media/", "/media/crop/600/400/");
 };
 //this is a function that takes in an image and returns a cropped image and make image uniform
